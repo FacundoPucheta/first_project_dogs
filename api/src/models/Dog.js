a759301b2eddcfcc96965d1b3d2b56410ce71888
Facundo Pucheta <facundopucheta94@gmail.com>
2023-05-11 17:31:46 -0300

    Models/Relations done + Server connected to DB

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -1,12 +1,45 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('dog', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "dog",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      weight: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      height: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      lifeSpan: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      belongToDb: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+        allowNull: false,
+      },
     },
-  });
+    {
+      timestamps: false,
+    }
+  );
 };
