54774021a3ff79586d3b6cb882001132eaa8a0bc
Facundo Pucheta <facundopucheta94@gmail.com>
2023-05-19 21:51:54 -0300

    paginated done

--- client/src/components/DogsContainer/DogsContainer.jsx
+++ client/src/components/DogsContainer/DogsContainer.jsx
@@ -1,26 +1,32 @@
 import DogCard from "../DogCard/DogCard";
 import style from "./DogsContainer.module.css";
-import { useSelector } from "react-redux";
 
-const DogsContainer = () => {
-
-  const defaultImg = "https://img.ecartelera.com/noticias/fotos/24800/24860/5.jpg";
-  const {dogs} = useSelector((state) => state.dogs);
+const DogsContainer = (props) => {
+  const defaultImg =
+    "https://img.ecartelera.com/noticias/fotos/24800/24860/5.jpg";
+  // const { dogs } = useSelector((state) => state.dogs);
 
   return (
-    <div className={style.container}>
-      {dogs?.map((dog) => {
-        return (
-          <DogCard
-            key={dog.id}
-            id={dog.id}
-            image={dog.image !== "" ? dog.image : defaultImg}
-            name={dog.name}
-            weight={dog.weight}
-            temperament={dog.temperament}
-          />
-        );
-      })}
+    <div>
+      <div>
+        <h1>Pagina {props.currentPage}</h1>
+        <button onClick={props.prevHandler}>Prev </button>
+        <button onClick={props.nextHandler}>Next </button>
+      </div>
+      <div className={style.container}>
+        {props.breeds.map((dog) => {
+          return (
+            <DogCard
+              key={dog.id}
+              id={dog.id}
+              image={dog.image !== "" ? dog.image : defaultImg}
+              name={dog.name}
+              weight={dog.weight}
+              temperament={dog.temperament}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,26 +1,61 @@
-import DogsContainer from "../../components/DogsContainer/DogsContainer";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
 import { getAllDogs } from "../../redux/actions";
+import DogsContainer from "../../components/DogsContainer/DogsContainer";
 
+const DOGS_RENDER = 8;
 
 const Home = () => {
-
   const dispatch = useDispatch();
-  const {dogs} = useSelector((state) => state.dogs);
 
-  useEffect(()=> {
+  useEffect(() => {
     dispatch(getAllDogs());
-  },[dispatch])
+  }, [dispatch]);
+  
+  let { dogs } = useSelector((state) => state.dogs);
+  
+  useEffect(() => {
+    if (Array.isArray(dogs) || dogs.length > 0) setBreeds([...dogs]?.splice(0, DOGS_RENDER));
+  }, [dogs]);
+  
+  if (!Array.isArray(dogs)) dogs = [];
+  
+  
+  const [breeds, setBreeds] = useState([...dogs]?.splice(0, DOGS_RENDER));
+  const [currentPage, setCurrentPage] = useState(0);
 
+  const prevHandler = () => {
+    if (currentPage === 0) return;
+    const prevPage = currentPage - 1;
+    const firstIndex = prevPage * DOGS_RENDER;
 
+    setBreeds([...dogs]?.splice(firstIndex, DOGS_RENDER));
+    setCurrentPage(prevPage);
+  };
 
-  return (
-    <>
+  const nextHandler = () => {
+    const dogsNumber = [...dogs].length;
+    const nextPage = currentPage + 1;
+    const firstIndex = nextPage * DOGS_RENDER;
+    
+    if(firstIndex >= dogsNumber) return;
+
+    setBreeds([...dogs]?.splice(firstIndex, DOGS_RENDER));
+    setCurrentPage(nextPage);
+  };
+
+  return !Array.isArray(dogs) || dogs.length === 0 ? (
+    <div>Loading...</div>
+  ) : (
+    <div>
       <h1>Esta es la vista de Home</h1>
-      {!dogs ? <div>Loading..</div> : <DogsContainer />}
-      
-    </>
+      <DogsContainer
+        prevHandler={prevHandler}
+        nextHandler={nextHandler}
+        breeds={breeds}
+        currentPage={currentPage}
+      />
+    </div>
   );
 };
 
