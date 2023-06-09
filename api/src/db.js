720cb056113d83f8c45fe50a1a02a3efa912af4f
Facundo Pucheta <facundopucheta94@gmail.com>
2023-06-09 16:13:45 -0300

    fix(/api env): new build

--- api/src/db.js
+++ api/src/db.js
@@ -5,7 +5,7 @@ const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
 
 const sequelize = new Sequelize(
-  DB_DEPLOY,
+  `${DB_DEPLOY}`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
@@ -18,6 +18,8 @@ const sequelize = new Sequelize(
 //     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 //   }
 // );
+
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
