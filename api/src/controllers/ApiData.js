3da0c8b1ca197eceb1c6fb1688c58bd64176dbd5
Facundo Pucheta <facundopucheta94@gmail.com>
2023-08-20 12:11:39 -0300

    fix(/index + /src/controllers/ApiData): fix_image
    New method to obtain image has been added

--- api/src/controllers/ApiData.js
+++ api/src/controllers/ApiData.js
@@ -2,13 +2,21 @@ require("dotenv").config();
 const { API_KEY } = process.env;
 const axios = require("axios");
 const URL = "https://api.thedogapi.com/v1/breeds";
+const imageURL = "https://api.thedogapi.com/v1/images";
 
-const getApiData = async () =>
-  await axios.get(`${URL}?${API_KEY}`).then(({ data }) => {
-    const dogsInfo = data.map((dog) => {
+const getDogData = async () => {
+
+  const response = await axios.get(URL, { headers: { "x-api-key": API_KEY} });
+  const dogs = response.data;
+  
+  const mappedDogs = await Promise.all(
+    dogs.map(async (dog) => {
+      const imageResponse = await axios.get(`${imageURL}/${dog.reference_image_id}`, { headers: {"x-api-key": API_KEY} } );
+      const image = imageResponse.data.url;
+
       return {
         id: dog.id,
-        image: dog.image?.url,
+        image: image,
         name: dog.name,
         weight: dog.weight.metric,
         height: dog.height.metric,
@@ -16,9 +24,11 @@ const getApiData = async () =>
         belongToDb: false,
         temperament: dog.temperament,
       };
-    });
-    if(!dogsInfo.length) throw Error ("Info cant be properly obtained.")
-    return dogsInfo;
-  });
+    })
+  );
 
-module.exports = getApiData;
+  if (!mappedDogs.length) throw Error("Info cant be properly obtained.");
+  return mappedDogs;
+};
+
+module.exports = getDogData;
